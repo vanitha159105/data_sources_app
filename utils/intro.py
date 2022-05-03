a6c247703ee7c71dd3d789a0cd8d57900b6ba982
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-03 12:33:48 -0700

    Update intro.py

--- utils/intro.py
+++ utils/intro.py
@@ -1,18 +1,49 @@
 import streamlit as st
+import tkinter as tk
+from tkinter import ttk
 
 TUTORIAL_URL = "https://docs.streamlit.io/en/latest/tutorial/databases.html"
 
 INTRO_IDENTIFIER = "—"
 
-HOME_PAGE_TEXT = f""" ## Welcome to the 🔌 Data Sources app!
-
-Congratulations, you have successfully forked and deployed this app 🎊  
-
-We'll now help you:
-- Collect your credentials and safely add them to your 🔑 Streamlit Secrets
-- Get started with your own app by providing you with sufficient code 🚀 
-
-**Ready?**
+HOME_PAGE_TEXT = f""" ## Capital Commission Application!
+  
+# Creating tkinter window
+window = tk.Tk()
+window.title('Combobox')
+window.geometry('500x250')
+  
+# label text for title
+ttk.Label(window, text = "GFG Combobox Widget", 
+          background = 'green', foreground ="white", 
+          font = ("Times New Roman", 15)).grid(row = 0, column = 1)
+  
+# label
+ttk.Label(window, text = "Select the Month :",
+          font = ("Times New Roman", 10)).grid(column = 0,
+          row = 5, padx = 10, pady = 25)
+  
+# Combobox creation
+n = tk.StringVar()
+monthchoosen = ttk.Combobox(window, width = 27, textvariable = n)
+  
+# Adding combobox drop down list
+monthchoosen['values'] = (' January', 
+                          ' February',
+                          ' March',
+                          ' April',
+                          ' May',
+                          ' June',
+                          ' July',
+                          ' August',
+                          ' September',
+                          ' October',
+                          ' November',
+                          ' December')
+  
+monthchoosen.grid(column = 1, row = 5)
+monthchoosen.current()
+window.mainloop()
 
 👈 Choose the data source you want to access!
 """
