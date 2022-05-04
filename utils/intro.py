a5c5449ee18ce2ba39dcfbe32f1ca2be99ab5b3a
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-03 17:09:49 -0700

    Update intro.py

--- utils/intro.py
+++ utils/intro.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import tkinter
-import tkinter as ttk
-from tkinter import *
+
 
 TUTORIAL_URL = "https://docs.streamlit.io/en/latest/tutorial/databases.html"
 
@@ -12,35 +10,10 @@ HOME_PAGE_TEXT = f""" ## Welcome to Capital Commission App!
 
  
  
-root = tkinter.Tk()
-root.title("Tk dropdown example")
- 
-# Add a grid
-mainframe = tkinter.Frame(root)
-mainframe.grid(column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S))
-mainframe.columnconfigure(0, weight = 1)
-mainframe.rowconfigure(0, weight = 1)
-mainframe.pack(pady = 100, padx = 100)
- 
-# Create a Tkinter variable
-tkvar = tkinter.StringVar(root)
- 
-# Dictionary with options
-choices = { 'PYTHON','C','C++','PHP','JAVA'}
-tkvar.set('PYTHON') # set the default option
- 
-popupMenu = tkinter.OptionMenu(mainframe, tkvar, *choices)
-tkinter.Label(mainframe, text="Choose Programming Language").grid(row = 1, column = 1)
-popupMenu.grid(row = 2, column =1)
- 
-# on change dropdown value
-def change_dropdown(*args):
-    print( tkvar.get() )
- 
-# link function to change dropdown
-tkvar.trace('w', change_dropdown)
- 
-root.mainloop()
+option = st.selectbox(
+...     'Select a form',
+...     ('Form 1', 'Form 2', 'Form 3'))
+ st.write('You selected:', option)
 **Ready?**
 
 👈 Choose the data source you want to access!
